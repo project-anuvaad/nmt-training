16cea22d088b8d7935b90f27814380924107dfad
aj7tesh <ajitesh1993@gmail.com>
2021-02-02 17:26:49 +0530

    train.sh updated

--- train.sh
+++ train.sh
@@ -3,6 +3,6 @@
 onmt_build_vocab -config config/en-bn.yaml -n_sample -1
 echo "********** Build vocab finished! ************"
 echo "********** Training Starting! ************"
-onmt_train -config config/en-bn.yaml
+nohup onmt_train -config config/en-bn.yaml &
 
 # tensorboard --bind_all --logdir='runs/onmt/' --port=3003
