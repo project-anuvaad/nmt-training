9543eafebaa27d432d74a41d91748715be1f846d
aj7tesh <ajitesh1993@gmail.com>
2021-02-08 18:35:37 +0530

    train.sh updated

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-onmt_build_vocab -config config/en-ta.yaml -n_sample -1
+onmt_build_vocab -config config/en-hi.yaml -n_sample -1
 echo "********** Build vocab finished! ************"
 echo "********** Training Starting! ************"
-nohup onmt_train -config config/en-ta.yaml &
+nohup onmt_train -config config/en-hi.yaml &
 
-# tensorboard --bind_all --logdir='runs/onmt/en-to-hi-aai4b-1' --port=3003
+# nohup tensorboard --bind_all --logdir='runs/onmt/en-to-hi-aai4b-1' --port=3003 &
