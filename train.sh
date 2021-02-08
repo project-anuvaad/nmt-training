4457143b42a005b55706d68c3adb17122ed644a9
aj7tesh <ajitesh1993@gmail.com>
2021-02-08 14:57:51 +0530

    en-hi training yaml added

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-onmt_build_vocab -config config/en-bn.yaml -n_sample -1
+onmt_build_vocab -config config/en-hi.yaml -n_sample -1
 echo "********** Build vocab finished! ************"
 echo "********** Training Starting! ************"
-nohup onmt_train -config config/en-bn.yaml &
+nohup onmt_train -config config/en-hi.yaml &
 
-# tensorboard --bind_all --logdir='runs/onmt/' --port=3003
+# tensorboard --bind_all --logdir='runs/onmt/en-to-hi-aai4b-1' --port=3003
