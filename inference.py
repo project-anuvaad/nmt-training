e6c5ab153fd9a2c4fedec78b6631299414026269
aj7tesh <ajitesh1993@gmail.com>
2021-02-21 10:02:34 +0530

    inference script updated

--- inference.py
+++ inference.py
@@ -16,21 +16,34 @@ def generate_inference(nmt_model,encoder_model,decoder_model,test_src,key,tgt_la
         output_decoded = os.path.join(prediction_folder, 'output_decoded'+'-'+key+'.txt')
         output_final = os.path.join(prediction_folder, 'output_final'+'-'+key+'.txt')        
         
-        os.system('perl ./tools/tokenizer.perl <{0}> {1}'.format(test_src, test_src_tok))
-        sp.encode_as_pieces(encoder_model,test_src_tok,test_src_encoded)
-        os.system('onmt_translate -model {0} -src {1} \
-                  -output {2} -verbose -beam_size 5 -gpu 0'.format(nmt_model,test_src_encoded,output))
-        sp.decode_as_pieces(decoder_model,output,output_decoded)  
-        os.system('python ./tools/indic_detokenize.py {0} {1} {2}'.format(output_decoded,output_final,tgt_lang_code)) 
-        
-        print("bleu operation begin")
-        nmt_out_tok_file = os.path.join(prediction_folder, 'nmt_out_tok_file'+'-'+key+'.txt')
-        ref_tok_file = os.path.join(prediction_folder, 'ref_tok_file'+'-'+key+'.txt')
-        os.system('python indic_tokenize.py {0} {1} {2}'.format(output_final,nmt_out_tok_file,tgt_lang_code))
-        os.system('python indic_tokenize.py {0} {1} {2}'.format(ref_file,ref_tok_file,tgt_lang_code))
-        os.system('sacrebleu --tokenize none {0} < {1}'.format(ref_tok_file,nmt_out_tok_file))
-        os.system('perl multi-bleu-detok.perl {0} < {1}'.format(ref_file,output_final))
-               
+        if tgt_lang_code in ['hi','bn','mr','ta','ml','gu','kn','te','pa']:
+            os.system('perl ./tools/tokenizer.perl <{0}> {1}'.format(test_src, test_src_tok))
+            sp.encode_as_pieces(encoder_model,test_src_tok,test_src_encoded)
+            os.system('onmt_translate -model {0} -src {1} \
+                    -output {2} -verbose -beam_size 5 '.format(nmt_model,test_src_encoded,output))
+            sp.decode_as_pieces(decoder_model,output,output_decoded)  
+            os.system('python ./tools/indic_detokenize.py {0} {1} {2}'.format(output_decoded,output_final,tgt_lang_code)) 
+            
+            print("bleu operation begin")
+            nmt_out_tok_file = os.path.join(prediction_folder, 'nmt_out_tok_file'+'-'+key+'.txt')
+            ref_tok_file = os.path.join(prediction_folder, 'ref_tok_file'+'-'+key+'.txt')
+            os.system('python tools/indic_tokenize.py {0} {1} {2}'.format(output_final,nmt_out_tok_file,tgt_lang_code))
+            os.system('python tools/indic_tokenize.py {0} {1} {2}'.format(ref_file,ref_tok_file,tgt_lang_code))
+            os.system('sacrebleu --tokenize none {0} < {1}'.format(ref_tok_file,nmt_out_tok_file))
+            os.system('perl tools/multi-bleu-detok.perl {0} < {1}'.format(ref_file,output_final))
+        elif tgt_lang_code =='en':    
+            os.system('python ./tools/indic_tokenize.py {0} {1} {2}'.format(input_file,output_file,lang_code))
+            sp.encode_as_pieces(encoder_model,test_src_tok,test_src_encoded)
+            os.system('onmt_translate -model {0} -src {1} \
+                    -output {2} -verbose -beam_size 5 '.format(nmt_model,test_src_encoded,output))
+            sp.decode_as_pieces(decoder_model,output,output_decoded)  
+            os.system('perl ./tools/detokenize.perl <{0}> {1} -l en'.format(output_decoded,output_final))
+            
+            print("bleu operation begin")
+            nmt_out_tok_file = os.path.join(prediction_folder, 'nmt_out_tok_file'+'-'+key+'.txt')
+            ref_tok_file = os.path.join(prediction_folder, 'ref_tok_file'+'-'+key+'.txt')
+            os.system('sacrebleu {0} < {1}'.format(ref_file,output_final))
+            os.system('perl tools/multi-bleu-detok.perl {0} < {1}'.format(ref_file,output_final))
         
     except Exception as e:
         print(e)    
